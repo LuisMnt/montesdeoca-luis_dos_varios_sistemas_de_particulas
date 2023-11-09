f38506849ae0937b6ba30e40f1e6e42f42e773af
LuisMnt <djlgkp501@gmail.com>
2023-11-09 15:19:05 -0500

    Updates

--- sketch.js
+++ sketch.js
@@ -5,3 +5,17 @@ function setup() {
 function draw() {
   circle(mouseX, mouseY, 20);
 }
+
+// let p;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   p = new Particula(width / 2, height / 2);
+// }
+
+// function draw() {
+//   if (p.isAlive) {
+//     p.update();
+//     p.display();
+//   }
+// }
